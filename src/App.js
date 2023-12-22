d7098d71d2eb44c6af620e0fbcc97a438b32a1ec
fullStackCoder2308 <sanskaraggarwal2025@gmail.com>
2023-12-22 16:27:50 +0530

    components, props, jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Video from './components/Video';
 function App() {
+  let obj = {
+    title:"MongoDb",
+    channel:"Sanskar",
+    views:"10K",
+    time:"1.5 Years"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      {/* We can pass props in any order  */}
+      <Video title = "ReactJS" channel="Sanskar" views="1k" time="1 year"></Video>
+      <Video title = "NodeJS" channel="Sanskar" views="10k" time="2 year"></Video>
+
+      {/* in the below component we use obj to pass props */}
+      <Video {...obj}></Video>
+    </>
   );
 }
 
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Video.css'
 
-const Video = () => {
+//for giving default values to props we can add it here itself 
+// like ({title = 'mongo'})
+const Video = ({title,channel,views,time}) => {
   return (
-    <div>Video</div>
+    <>
+        <div className='container'>
+            <div>
+                <img src = "https://loremflickr.com/180/180" />
+            </div>
+            <div>
+                <p>{title}</p>
+                <p>{channel}</p>
+                <p>{views}</p>
+                <p>{time}</p>
+            </div>
+        </div>
+    </>
+
   )
 }
 
